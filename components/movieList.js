dcbc105dd81fd464c09b3a8fe2db2daffac64751
Yevgeniy Goncharov <g.yevgeniy.p@gmail.com>
2020-01-08 18:36:12 +0300

    separate MOVIE_DB to actions/index

--- components/movieList.js
+++ components/movieList.js
@@ -38,34 +38,47 @@ class MovieList extends React.Component {
     //
     // }
 
+    shorten = (text, maxLengh) => {
+        if (text && text.length > maxLengh)
+        {
+            return text.substr(0, maxLengh) + '...'
+        }
+        return text
+    }
+
+    renderMovies() {
+        const { movies } = this.props
+
+        return movies.map(movie =>
+            (
+
+                <div key={movie.id} className="col-lg-4 col-md-6 mb-4">
+                    <div className="card h-100">
+                        <a href="#"><img className="card-img-top" src={movie.image} alt=""/></a>
+                        <div className="card-body">
+                            <h4 className="card-title">
+                                <a href="#">{movie.name}</a>
+                            </h4>
+                            <p className="card-text">{this.shorten(movie.description, 180)}</p>
+                        </div>
+                        <div className="card-footer">
+                            <small className="text-muted">{movie.rating}</small>
+                        </div>
+                    </div>
+                </div>
+            )
+        )
+    }
 
     render() {
         // debugger
 
-        //const movies = this.props.movies
+        // const movies = this.props.movies
         // OR can use
-        const { movies } = this.props
-
+        // const { movies } = this.props
         return(
             <React.Fragment>
-                { movies.map((movie) => {
-                    return(
-                        <div className="col-lg-4 col-md-6 mb-4">
-                            <div className="card h-100">
-                                <a href="#"><img className="card-img-top" src={movie.image} alt=""/></a>
-                                <div className="card-body">
-                                    <h4 className="card-title">
-                                        <a href="#">{movie.name}</a>
-                                    </h4>
-                                    <p className="card-text">{movie.description}</p>
-                                </div>
-                                <div className="card-footer">
-                                    <small className="text-muted">{movie.rating}</small>
-                                </div>
-                            </div>
-                        </div>
-                    )
-                }) }
+                { this.renderMovies() }
 
             </React.Fragment>
         )
